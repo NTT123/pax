604c0c8907840c77fd85d47cb5231a962717ea94
NTT123 <tanthong.nguyen@ntu.edu.sg>
2021-10-01 22:50:28 +0800

    v0.3.1rc

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ copyright = "2021, Thông Nguyễn"
 author = "Thông Nguyễn"
 
 # The full version, including alpha/beta/rc tags
-release = "v0.3.0"
+release = "v0.3.1rc"
 
 
 # -- General configuration ---------------------------------------------------
