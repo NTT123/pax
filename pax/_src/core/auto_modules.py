835242f65ffaca58c52cdc0c806a4ad3d57e678c
NTT123 <9960521+NTT123@users.noreply.github.com>
2021-10-28 07:45:28 +0000

    Check wrong kind in ParameterModule and StateModule

--- pax/_src/core/auto_modules.py
+++ pax/_src/core/auto_modules.py
@@ -1,7 +1,7 @@
 """Modules with default kinds"""
 
 
-from typing import Type, TypeVar
+from typing import Iterable, Type, TypeVar
 
 from .base import PaxKind, allow_mutation
 from .module import Module
@@ -18,6 +18,17 @@ class ParameterModule(Module):
             module._update_default_kind(PaxKind.PARAMETER)
         return module
 
+    def _scan_fields(self, fields: Iterable[str]):
+        for field_name in fields:
+            if field_name in self._pax.name_to_kind:
+                kind = self._pax.name_to_kind[field_name]
+                if kind != PaxKind.PARAMETER:
+                    raise ValueError(
+                        f"`{self.__class__.__name__}.{field_name}` has kind `{kind.name}`. "
+                        f"This is not allowed for a ParameterModule object."
+                    )
+        return super()._scan_fields(fields)
+
 
 class StateModule(Module):
     """A PAX module that registers STATE by default"""
@@ -27,3 +38,14 @@ class StateModule(Module):
         with allow_mutation(module):
             module._update_default_kind(PaxKind.STATE)
         return module
+
+    def _scan_fields(self, fields: Iterable[str]):
+        for field_name in fields:
+            if field_name in self._pax.name_to_kind:
+                kind = self._pax.name_to_kind[field_name]
+                if kind != PaxKind.STATE:
+                    raise ValueError(
+                        f"`{self.__class__.__name__}.{field_name}` has kind `{kind.name}`. "
+                        f"This is not allowed for a StateModule object."
+                    )
+        return super()._scan_fields(fields)
---
+++ tests/test_auto_modules.py
@@ -0,0 +1,23 @@
+import jax.numpy as jnp
+import pax
+import pytest
+
+
+def test_scan_bug_param_module():
+    class M(pax.ParameterModule):
+        def __init__(self):
+            super().__init__()
+            self.register_state("a", jnp.array(0.0))
+
+    with pytest.raises(ValueError):
+        m = M()
+
+
+def test_scan_bug_state_module():
+    class M(pax.StateModule):
+        def __init__(self):
+            super().__init__()
+            self.register_parameter("a", jnp.array(0.0))
+
+    with pytest.raises(ValueError):
+        m = M()
