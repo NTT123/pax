850c898fb8bbd843bc4d48b06ab708f415fb4f1f
NTT123 <9960521+NTT123@users.noreply.github.com>
2021-10-29 08:46:27 +0000

    Improve auto module docs and example

--- examples/auto_module.py
+++ examples/auto_module.py
@@ -1,3 +1,6 @@
+from dataclasses import dataclass
+from typing import Sequence
+
 import jax
 import jax.numpy as jnp
 import pax
@@ -5,31 +8,17 @@ import pax
 pax.seed_rng_key(42)
 
 
-class M(pax.AutoModule):
+@dataclass
+class MLP(pax.AutoModule):
+    features: Sequence[int]
+
     def __call__(self, x):
-        x = self.get_or_create("fc1", lambda: pax.nn.Linear(3, 3))(x)
-        x = jax.nn.relu(x)
-        x = self.get_or_create("fc2", lambda: pax.nn.Linear(3, 5))(x)
-        return x
-
-    def inference(self, x):
-        x = self.fc1(x)
-        x = jax.nn.relu(x)
-        x = self.fc2(x)
+        sizes = zip(self.features[:-1], self.features[1:])
+        for i, (in_dim, out_dim) in enumerate(sizes):
+            fc = self.get_or_create(f"fc_{i}", lambda: pax.nn.Linear(in_dim, out_dim))
+            x = jax.nn.relu(fc(x))
         return x
 
 
-def loss_fn(net: M, x, y):
-    net, y_hat = net % x
-    loss = jnp.mean(jnp.square(y - y_hat))
-    return loss, net
-
-
-x = jnp.ones((32, 3))
-y = jnp.ones((32, 5))
-net, _ = M() % x
-
-print(net.summary())
-y = net.inference(x)
-y = net(x)
-print(y.shape)
+mlp, _ = MLP([1, 2, 3, 4, 5]) % jnp.ones((1, 1))
+print(mlp.summary())
--- pax/_src/core/auto_modules.py
+++ pax/_src/core/auto_modules.py
@@ -30,7 +30,31 @@ class StateModule(Module):
 
 
 class AutoModule(Module):
-    """A module that auto creates a submodule when needed."""
+    """A module that auto creates a submodule when needed.
+
+
+    Example:
+
+    >>> @dataclass
+    ... class MLP(pax.AutoModule):
+    ...     features: Sequence[int]
+    ...
+    ...     def __call__(self, x):
+    ...         sizes = zip(self.features[:-1], self.features[1:])
+    ...         for i, (in_dim, out_dim) in enumerate(sizes):
+    ...             fc = self.get_or_create(f"fc_{i}", lambda: pax.nn.Linear(in_dim, out_dim))
+    ...             x = jax.nn.relu(fc(x))
+    ...         return x
+    ...
+    ...
+    >>> mlp, _ = MLP([1, 2, 3, 4, 5]) % jnp.ones((1, 1))
+    >>> print(mlp.summary())
+    MLP(features=[1, 2, 3, 4, 5])
+    ├── Linear[in_dim=1, out_dim=2, with_bias=True]
+    ├── Linear[in_dim=2, out_dim=3, with_bias=True]
+    ├── Linear[in_dim=3, out_dim=4, with_bias=True]
+    └── Linear[in_dim=4, out_dim=5, with_bias=True]
+    """
 
     def get_or_create(self, name, create_fn: Callable[[], Module]):
         """Create and register a new module when it is not an attribute of the module."""
