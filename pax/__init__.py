4169fc3f431b134e3795cdf69966946f0b1692ee
NTT123 <9960521+NTT123@users.noreply.github.com>
2021-11-18 15:58:37 +0000

    Update __init__ files.

--- pax/__init__.py
+++ pax/__init__.py
@@ -4,7 +4,6 @@ from pax import experimental, nets, nn, utils
 from pax._src.core import (
     Module,
     ParameterModule,
-    PaxKind,
     StateModule,
     apply_mp_policy,
     assert_structure_equal,
@@ -12,22 +11,16 @@ from pax._src.core import (
     enable_train_mode,
     freeze_parameters,
     module_and_value,
+    parameters_method,
     pure,
     select_parameters,
-    select_states,
     unfreeze_parameters,
     unwrap_mp_policy,
     update_parameters,
-    update_states,
 )
 from pax._src.core.rng import next_rng_key, seed_rng_key
 from pax._src.nn.dropout import dropout
 from pax._src.utils import build_update_fn, grad, scan, value_and_grad
-
-STATE = PaxKind.STATE
-PARAMETER = PaxKind.PARAMETER
-P = PaxKind.PARAMETER
-S = PaxKind.STATE
 
 __version__ = "0.4.4.dev"
 
@@ -47,17 +40,15 @@ __all__ = (
     "next_rng_key",
     "nn",
     "ParameterModule",
-    "PaxKind",
+    "parameters_method",
     "pure",
     "scan",
     "seed_rng_key",
     "select_parameters",
-    "select_states",
     "StateModule",
     "unfreeze_parameters",
     "unwrap_mp_policy",
     "update_parameters",
-    "update_states",
     "utils",
     "value_and_grad",
 )
