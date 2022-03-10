4c5f04ec6fb3b970ea4c7bedb7e6ef8379552a92
NTT123 <9960521+NTT123@users.noreply.github.com>
2022-03-10 06:37:56 +0000

    v0.5.6

--- pax/__init__.py
+++ pax/__init__.py
@@ -51,7 +51,7 @@ from pax._src.nn import (
 from pax._src.nn.dropout import dropout
 from pax._src.utils import build_update_fn, grad, scan, value_and_grad
 
-__version__ = "0.5.5"
+__version__ = "0.5.6"
 
 __all__ = (
     "apply_mp_policy",
